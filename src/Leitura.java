4165177a55addfe80d02a84eae619c4f7ea94bd5
Vinicius Borba <vinichius@gmail.com>
2017-10-18 16:08:23 -0200

    Classe Litura está separando por vírgula as linhas

--- src/Leitura.java
+++ src/Leitura.java
@@ -5,16 +5,26 @@ public class Leitura {
     public static void main(String[] args) throws IOException {
 
         Scanner s = null;//Objeto Scanner?
+        
+        
+		
 
         try {
-            s = new Scanner(new BufferedReader(new FileReader("/home/vini/Imagens/teste.txt")));
+            s = new Scanner(new BufferedReader(new FileReader("/home/vini/Imagens/participantes.csv")));
    //Como que um objeto Scanner poder ser tudo isso?
    // aqui que o 's' vira um objeto de Scanner?
-   //e pq e preciso um FileReader dentro de um BufferReader? So o Buffer nÃ�Â£o Ã�Â© suficiente?
+   //e pq e preciso um FileReader dentro de um BufferReader? So o Buffer nao e suficiente?
 
             while (s.hasNext()) {
+            	int c = 0;
+            	String[] arrayStrings = new String[c];
    //objeto.hasNext() "nao sei que esse metodo faz"
+            	//s.reset();
+                //System.out.println("" + s.findInLine(","));
+            	
                 System.out.println(s.next());
+                System.out.println("" + s.findInLine(","));
+                
                 //imprime o objeto.next() "esse metodo .next() existe em diferentes classes?"
                 //Se for, acho que o next() quebra a cada espacoo em branco.
                 //Mas nao sei como isso acontece
