e24c56c29d228962932839b0295516a311c6ea08
Vinicius Borba <vinichius@gmail.com>
2017-10-18 16:54:16 -0200

    Leitura.java lẽ o arqquivo, e transforma no e e email e objeto, ma segue aparecendo o null

--- src/Leitura.java
+++ src/Leitura.java
@@ -5,7 +5,8 @@ public class Leitura {
     public static void main(String[] args) throws IOException {
 
         Scanner s = null;//Objeto Scanner?
-        
+        int c = 0;
+    	String[] arrayStrings = new String[20];
         
 		
 
@@ -18,17 +19,26 @@ public class Leitura {
             while (s.hasNext()) {
             	//int c = 0;
             	//String[] arrayStrings = new String[c];
-   //objeto.hasNext() "nao sei que esse metodo faz"
+            	//objeto.hasNext() "nao sei que esse metodo faz"
             	//s.reset();
                 //System.out.println("" + s.findInLine(","));
             	
-                System.out.println(s.next() + s.findInLine(","));
+                //System.out.println(s.next() + s.findInLine(","));
+                arrayStrings[c] = s.next() + s.findInLine(",");
                 //System.out.println(s.findInLine(","));
-                
+                //System.out.println(arrayStrings.length);
+                c = c + 1;
                 //imprime o objeto.next() "esse metodo .next() existe em diferentes classes?"
                 //Se for, acho que o next() quebra a cada espacoo em branco.
                 //Mas nao sei como isso acontece
             }
+            System.out.println(arrayStrings[0]);
+            System.out.println(arrayStrings[1]);
+            
+            //for(int i = 0; i < 11; i ++){
+				//System.out.println(arrayStrings[i]);
+            //}
+            
         } finally {
             if (s != null) {
                 s.close();
