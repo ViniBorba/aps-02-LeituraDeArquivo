8d2b02cf0b16a71fa607e096ca8ee5417b991a69
Vinicius Borba Santos <vinichius@gmail.com>
2017-10-18 21:40:56 -0200

    Agora quebra com espaço e com vírgula

--- src/Leitura.java
+++ src/Leitura.java
@@ -11,7 +11,7 @@ public class Leitura {
 		
 
         try {
-            s = new Scanner(new BufferedReader(new FileReader("/home/vini/Imagens/participantes.csv")));
+            s = new Scanner(new BufferedReader(new FileReader("C:\\Users\\631420217\\workspace\\participantes.csv"))).useDelimiter(",");
    //Como que um objeto Scanner poder ser tudo isso?
    // aqui que o 's' vira um objeto de Scanner?
    //e pq e preciso um FileReader dentro de um BufferReader? So o Buffer nao e suficiente?
@@ -22,18 +22,18 @@ public class Leitura {
             	//objeto.hasNext() "nao sei que esse metodo faz"
             	//s.reset();
                 //System.out.println("" + s.findInLine(","));
-            	
+            	System.out.println(s.next());
                 //System.out.println(s.next() + s.findInLine(","));
-                arrayStrings[c] = s.next() + s.findInLine(",");
+                //arrayStrings[c] = s.next() + s.findInLine(",");
                 //System.out.println(s.findInLine(","));
                 //System.out.println(arrayStrings.length);
-                c = c + 1;
+                //c = c + 1;
                 //imprime o objeto.next() "esse metodo .next() existe em diferentes classes?"
                 //Se for, acho que o next() quebra a cada espacoo em branco.
                 //Mas nao sei como isso acontece
             }
-            System.out.println(arrayStrings[0]);
-            System.out.println(arrayStrings[1]);
+            //System.out.println(arrayStrings[0]);
+            //System.out.println(arrayStrings[1]);
             
             //for(int i = 0; i < 11; i ++){
 				//System.out.println(arrayStrings[i]);
